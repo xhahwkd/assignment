03c48f62865329c7537c2be0671a5eda81baf9b3
xhahwkd <142880394+xhahwkd@users.noreply.github.com>
2025-06-27 12:12:16 +0900

    Update shoppingmall_sample.dart

--- shoppingmall_sample.dart
+++ shoppingmall_sample.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 
 class ShoppingMall {
   int totalPrice = 0;
+  Map<String, int> cart = {};
+
   menu_bar() {
     print('-' * 93);
     print(
-      "[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료",
+      "[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 장바구니 초기화 / [5] 프로그램 종료",
     );
     print('-' * 93);
     String? input = stdin.readLineSync();
@@ -29,9 +31,8 @@ class ShoppingMall {
   }
 
   addToCart(productName, productCount) {
-    int count = int.parse(productCount);
-    int price = productList[productName]!;
-    totalPrice += price * count;
+    cart[productName] = int.parse(productCount);
+    totalPrice += productList[productName]! * int.parse(productCount);
     return totalPrice;
   }
 }
@@ -70,11 +71,38 @@ void main() {
           break;
         }
       case '3':
-        print('장바구니에 ${shoppingMall.totalPrice}원 어치를 담으셨네요 !');
+        if (shoppingMall.cart.isEmpty) {
+          print('장바구니에 담긴 상품이 없습니다.');
+        } else {
+          print(
+            '장바구니에 ${shoppingMall.cart.keys.join(', ')}가 담겨있네요. 총 ${shoppingMall.totalPrice}원 입니다!',
+          );
+        }
         break;
       case '4':
-        print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
-        isRunning = false;
+        if (shoppingMall.cart.isNotEmpty) {
+          print('장바구니를 초기화합니다.');
+          shoppingMall.cart.clear();
+          shoppingMall.totalPrice = 0;
+        } else {
+          print('이미 장바구니가 비어있습니다.');
+        }
+        break;
+      case '5':
+        while (true) {
+          print('정말 종료하시겠습니까? (Y/N)');
+          String? confirm = stdin.readLineSync();
+          if (confirm == 'Y' || confirm == 'y') {
+            print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
+            isRunning = false;
+            break;
+          } else if (confirm == 'N' || confirm == 'n') {
+            print('종료하지 않습니다.');
+            break;
+          } else {
+            print('Y 또는 N으로 입력해 주세요.');
+          }
+        }
         break;
       default:
         print('지원하지 않는 기능입니다 ! 다시 시도해 주세요 ..');
